19c568782240d3ea5c73b8e2f3d6ee1c705769e2
PremaLatha2433 <premalatha2433@gmail.com>
2025-03-06 20:59:06 +0530

    router with process method.

--- src/main/java/com/apache/camel/ApacheCamelDemo/FileTransferRouter.java
+++ src/main/java/com/apache/camel/ApacheCamelDemo/FileTransferRouter.java
@@ -1,12 +1,30 @@
 package com.apache.camel.ApacheCamelDemo;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.StringTokenizer;
 
 @Component
 public class FileTransferRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("file:C:\\source").to("file:C:\\desti");
+        from("file:C:\\source") .process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Message input = exchange.getIn();
+                String data = input.getBody(String.class);
+                StringTokenizer str = new StringTokenizer(data,",");
+                String eId = str.nextToken();
+                String ename = str.nextToken();
+                String esal = str.nextToken();
+                String dataModified ="{eid:"+eId+",ename:"+ename+",esal:"+esal+"}";
+                Message output = exchange.getMessage();
+                output.setBody(dataModified);
+            }
+        }).to("file:C:\\desti?fileName=emp.json");
     }
 }
